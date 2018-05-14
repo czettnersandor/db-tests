2ffa0545d66702f17ae2c3318cf885ec7ef70b1a
Sándor Czettner <czettnersandor@users.noreply.github.com>
2018-05-14 17:19:38 +0100

    Fix code formatting issues

--- test2/function_disease_fetch.php
+++ test2/function_disease_fetch.php
@@ -1,58 +1,28 @@
 <?php
 
-
-
 include "connect_db.php";
-			function disease_fetch (){
+function disease_fetch () {
+	global $con;
+	$query="SELECT * FROM disease";
+	$result= mysqli_query($con, $query);
 
-			global $con;
-
-			$query="SELECT * FROM disease";
-					$result= mysqli_query($con, $query);
-
-					while($row = mysqli_fetch_assoc($result)){
-
-
-						$Name= $row['Name'];
-
-						echo "<option value='$Name'>$Name</option>";
-
-					}
+	while ($row = mysqli_fetch_assoc($result)) {
+		$Name= $row['Name'];
+		echo "<option value='$Name'>$Name</option>";
+	}
 }
 
+function diseases_all() {
+	global $con;
 
-?>
+	$query="select * from disease";
+	$result=mysqli_query($con, $query);
 
-
-
-
-
-
-<?php
-
-function diseases_all(){
-
-global $con;
-
-$query="select * from disease";
-$result=mysqli_query($con, $query);
-
-
-while ($row= mysqli_fetch_assoc($result)) {
-
-
-	$Code= $row['Code'];
-	$Name= $row['Name'];
-	$Description= $row['Description'];
-	$Treatment= $row['Treatment'];
-	
-
-
-	echo $Code . "-" . "-" . $Name . "<br>" . "<br>" . $Description . "<br>" . "<br>" . $Treatment . "<br>" . "<br>";
+	while ($row= mysqli_fetch_assoc($result)) {
+		$Code= $row['Code'];
+		$Name= $row['Name'];
+		$Description= $row['Description'];
+		$Treatment= $row['Treatment'];
+		echo $Code . "-" . "-" . $Name . "<br>" . "<br>" . $Description . "<br>" . "<br>" . $Treatment . "<br>" . "<br>";
+	}
 }
-
-
-
-}
-
-?>
